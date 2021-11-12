c077ec7ad84f7dd7c3cdef16fd09200f75932962
natacha <nat_bartu@hotmail.com>
2021-11-12 13:42:15 +1100

    add bootstrap dependency and import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <div >
-          The basic empty template
+    <div>
+      <h2>Portfolio Test</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
