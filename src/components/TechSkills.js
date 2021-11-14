8d52c1af4178502edc53418646c1f1cae9057fc0
natacha <nat_bartu@hotmail.com>
2021-11-14 19:02:59 +1100

    add icon

--- src/components/TechSkills.js
+++ src/components/TechSkills.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
+import { AiFillGithub, AiFillLinkedin, AiOutlineHtml5 } from "react-icons/ai";
+import { FaBootstrap } from "react-icons/fa";
+import { GrReactjs } from "react-icons/gr";
 import logo from "../images/logo.png";
+
 export const TechSkills = () => {
   return (
     <Container>
@@ -58,6 +62,24 @@ export const TechSkills = () => {
             </Col>
           </Row>
         </Row>
+      </Container>
+      <br />
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <AiFillGithub size="2rem" />
+
+        <AiFillLinkedin size="2rem" />
+
+        <FaBootstrap size="2rem" />
+
+        <GrReactjs size="2rem" />
+
+        <AiOutlineHtml5 size="2rem" />
       </Container>
     </Container>
   );
