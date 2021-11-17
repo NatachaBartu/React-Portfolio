f5b70af50f429a2035b047aeb4e6650c9a95fcea
natacha <nat_bartu@hotmail.com>
2021-11-17 16:07:44 +1100

    add images for the cards background

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
-import logo from "../images/logo.png";
+//import logo from "../images/logo.png";
+import meme from "../images/meme.png";
+import spaceUp from "../images/space-up.png";
+import open from "../images/open.png";
+import GitHub from "../images/GitHub.png";
 
 export function Projects() {
   return (
@@ -15,14 +19,22 @@ export function Projects() {
                 padding: "7px",
               }}
             >
-              <Card.Img variant="top" src={logo} />
+              <Card.Img variant="top" src={open} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>My First Portfolio</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  In my first portfolio, I had an excellent opportunity to
+                  handle pure HTML, Sass and CSS. I still need a bit to touch,
+                  but I left to see my improvement as I created new portfolios
+                  in another framework for comparing.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button
+                  variant="outline-dark"
+                  href="http://natacha-bartu.netlify.app"
+                >
+                  {" "}
+                  My First Portfolio
+                </Button>
               </Card.Body>
             </Card>
           </Col>
@@ -34,14 +46,20 @@ export function Projects() {
                 padding: "7px",
               }}
             >
-              <Card.Img variant="top" src={logo} />
+              <Card.Img variant="top" src={spaceUp} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Ruby on Rails</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  On Ruby on Rails, I learned how to use database, tables,
+                  backend and frontend during the build app. The project was
+                  quite fun.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button
+                  variant="outline-dark"
+                  href="https://github.com/NatachaBartu/space-up"
+                >
+                  Space Up Project
+                </Button>
               </Card.Body>
             </Card>
           </Col>
@@ -53,14 +71,20 @@ export function Projects() {
                 padding: "7px",
               }}
             >
-              <Card.Img variant="top" src={logo} />
+              <Card.Img variant="top" src={meme} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Hackathon Group Project</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Hackathon was a fun project that I learned how to work as a
+                  group on a build process of an application. It was built in
+                  React Platform, Git and GitHub tools and some Bootstrap.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button
+                  variant="outline-dark"
+                  href="https://github.com/NatachaBartu/hackathon_memeGenerator"
+                >
+                  Hackathon
+                </Button>
               </Card.Body>
             </Card>
           </Col>
@@ -72,14 +96,16 @@ export function Projects() {
                 padding: "7px",
               }}
             >
-              <Card.Img variant="top" src={logo} />
+              <Card.Img variant="top" src={GitHub} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>GitHub Challenges</Card.Title>
+                <Card.Text>Coming Soon!</Card.Text>
+                <Button
+                  variant="outline-dark"
+                  href="https://github.com/NatachaBartu/Challenges"
+                >
+                  GitHub Challenges
+                </Button>
               </Card.Body>
             </Card>
           </Col>
