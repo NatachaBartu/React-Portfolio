baf40de62fa6b2d1d2127459724aab2988202e67
natacha <nat_bartu@hotmail.com>
2021-11-21 17:15:44 +1100

    add color into icons, fix grid

--- src/pages/TechSkills.js
+++ src/pages/TechSkills.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { AiOutlineHtml5 } from "react-icons/ai";
-import { DiCss3Full, DiHeroku } from "react-icons/di";
+import { DiCss3Full, DiHeroku, DiRuby } from "react-icons/di";
 import { FaBootstrap } from "react-icons/fa";
 import { GrReactjs } from "react-icons/gr";
 import { SiNetlify, SiPostgresql } from "react-icons/si";
@@ -9,7 +9,7 @@ import { VscCode } from "react-icons/vsc";
 
 export const TechSkills = () => {
   return (
-    <Container>
+    <Container className="mx-auto text-center" style={{ marginTop: "40px" }}>
       <h1>My Tech Skills!</h1>
       <br />
       <p>
@@ -19,43 +19,48 @@ export const TechSkills = () => {
       <br />
 
       <Container>
-        <Row>
+        <Row xs={6} md={4}>
           {[
             {
-              image: <SiNetlify size="5rem" />,
+              image: <SiNetlify size="5rem" color="#E86565" />,
               title: "Netlify",
             },
             {
-              image: <SiPostgresql size="5rem" />,
+              image: <SiPostgresql size="5rem" color="#E86565" />,
               title: "Postgresql",
             },
             {
-              image: <AiOutlineHtml5 size="5rem" />,
+              image: <AiOutlineHtml5 size="5rem" color="#E86565" />,
               title: "HTML",
             },
             {
-              image: <DiCss3Full size="5rem" />,
+              image: <DiCss3Full size="5rem" color="#E86565" />,
               title: "Css",
             },
             {
-              image: <DiHeroku size="5rem" />,
+              image: <DiHeroku size="5rem" color="#E86565" />,
               title: "Heroku",
             },
             {
-              image: <FaBootstrap size="5rem" />,
+              image: <FaBootstrap size="5rem" color="#E86565" />,
               title: "Bootstrap",
             },
             {
-              image: <GrReactjs size="5rem" />,
+              image: <GrReactjs size="5rem" color="#E86565" />,
               title: "React",
             },
             {
-              image: <VscCode size="5rem" />,
+              image: <VscCode size="5rem" color="#E86565" />,
               title: "VsCode",
             },
+            {
+              image: <DiRuby size="5rem" color="#E86565" />,
+              title: "Ruby",
+            },
           ].map((item) => (
-            <Col style={{ margin: "5px" }}>
-              {item.image} {item.title}
+            <Col style={{ margin: "15px" }}>
+              {item.image}
+              <Col>{item.title}</Col>
             </Col>
           ))}
         </Row>
