5cd4cca29b7b57c16cf73f0cc91930faf69df0d0
natacha <nat_bartu@hotmail.com>
2021-11-21 17:51:01 +1100

    add stick position to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,11 @@ export const Footer = () => {
         className="d-flex text-center justify-content-end"
         bg="dark"
         variant="dark"
+        style={{
+          position: "absolute",
+          bottom: "0",
+          width: "100%",
+        }}
       >
         <Card.Footer style={{ color: "white" }}>
           &copy;{new Date().getFullYear()} Natacha Bartu | All right reserved
